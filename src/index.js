439673dca95c3c120fc8703bf3416d1133920d69
evxeen <evxeen@gmail.com>
2022-09-17 19:49:39 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import bodyParser from 'body-parser';
+// import {AppDataSource} from "./db.js";
+// import router from "./router/appRouter.js";
+
+const app = express();
+const PORT = process.env.PORT || 9001;
+app.use(bodyParser.json());
+app.use(cors());
+// app.use('/api', router);
+
+const start = async () => {
+   try {
+      // await AppDataSource.initialize()
+          // .then(() => {
+          //    console.log('база данных подключена');
+          // })
+          // .catch((error) => console.log(error));
+
+      app.listen(PORT, () => {
+         console.log(`сервер запущен на порту ${PORT}`);
+      });
+   } catch (e) {
+      console.log(e);
+   }
+};
+
+start();
+
